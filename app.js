d0d8cf5e32d6daf3a8b86100bfc749c741cb1b02
Ayesha Siddika <ayesha2k4as@gmail.com>
2025-08-16 18:55:55 +0530

    latest commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const env = require("dotenv").config();
+require("dotenv").config();
 const path = require('path');
 const express = require('express');
 const { generateWebsiteCode } = require("./aiAgent");
@@ -7,21 +7,19 @@ const app = express();
 const userRouter = require('./routes/userRouter');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-const rootDir = require('./utils/pathUtil');
-
-app.use(express.static(path.join(rootDir,'public')));
 app.use(userRouter);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(rootDir, "public", "index.html"));
+app.use((req, res) => {
+  res.status(404).send("Page not found");
 });
 
-const PORT = 3000;
-app.listen(PORT,()=>{
-  console.log(`Server is running on: http://localhost:${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
