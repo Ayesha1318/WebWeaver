e2461a7f24e160c8930d6626f9a9db04978409ac
Ayesha Siddika <ayesha2k4as@gmail.com>
2025-08-16 18:17:48 +0530

    latest commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const env = require("dotenv").config();
 const path = require('path');
 const express = require('express');
-const { generateWebsiteCode } = require("../aiAgent");
+const { generateWebsiteCode } = require("./aiAgent");
 
 const app = express();
-const userRouter = require('../routes/userRouter');
+const userRouter = require('./routes/userRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-const rootDir = require('../utils/pathUtil');
+const rootDir = require('./utils/pathUtil');
 app.use(userRouter);
 
 app.use(express.static(path.join(rootDir,'public')));
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT,()=>{
   console.log(`Server is running on: http://localhost:${PORT}`);
 })
 
-module.exports = app;
+// module.exports = app;
